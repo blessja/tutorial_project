9918c64d9bcc6ddea66b7bfcc878091e9d00afd7
Blessing Jackson <jaydenj772@gmail.com>
2024-04-08 18:42:52 +0200

    added customs styless and new makup for the home page

---
+++ public/js/custom.js
@@ -0,0 +1,35 @@
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "500",
+    width: "900",
+    videoId: "6LYMlWvDBIA",
+    playerVars: {
+      autoplay: 1,
+      controls: 1,
+      rel: 0,
+      showinfo: 0,
+    },
+  });
+}
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
